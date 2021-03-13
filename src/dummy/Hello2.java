2f381d0a3bf85fef85add0bb088f3f88c434f10b
satya sir <sathya.campusglues@gmail.com>
2021-03-14 01:04:19 +0530

    hello2

---
+++ src/dummy/Hello2.java
@@ -0,0 +1,10 @@
+package dummy;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("github hello satya sir");
+	}
+
+}
